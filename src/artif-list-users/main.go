42db2daffc8c042976a430c8875e32d67120965a
John E. Vincent <lusis.org+github.com@gmail.com>
2016-08-10 14:04:56 -0400

    adding the new cli options for list-users

--- src/artif-list-users/main.go
+++ src/artif-list-users/main.go
@@ -6,22 +6,43 @@ import (
 
 	artifactory "artifactory.v401"
 	"github.com/olekukonko/tablewriter"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	formatUsage = fmt.Sprintf("Format to show results [table, csv, list (usernames only - useful for piping)]")
+	format      = kingpin.Flag("format", formatUsage).Short('F').Default("table").Enum("table", "list", "csv")
+	sep         = kingpin.Flag("separator", "separator for csv output").Default(",").String()
 )
 
 func main() {
+	kingpin.UsageTemplate(kingpin.CompactUsageTemplate).Version("1.0").Author("John E. Vincent")
+	kingpin.CommandLine.Help = "List all users in Artifactory"
+	kingpin.Parse()
+
 	client := artifactory.NewClientFromEnv()
 	data, err := client.GetUsers()
 	if err != nil {
 		fmt.Printf("%s\n", err)
 		os.Exit(1)
 	} else {
-		table := tablewriter.NewWriter(os.Stdout)
-		table.SetHeader([]string{"Name", "Uri"})
-		table.SetAutoWrapText(false)
-		for _, u := range data {
-			table.Append([]string{u.Name, u.Uri})
+		if *format == "table" {
+			table := tablewriter.NewWriter(os.Stdout)
+			table.SetHeader([]string{"Name", "Uri"})
+			table.SetAutoWrapText(false)
+			for _, u := range data {
+				table.Append([]string{u.Name, u.Uri})
+			}
+			table.Render()
+		} else if *format == "list" {
+			for _, u := range data {
+				fmt.Printf("%s\n", u.Name)
+			}
+		} else if *format == "csv" {
+			for _, u := range data {
+				fmt.Printf("%s%s%s\n", u.Name, *sep, u.Uri)
+			}
 		}
-		table.Render()
 		os.Exit(0)
 	}
 }

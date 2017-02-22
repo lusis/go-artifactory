f0b2833bed5505d7fb19c713c7c425cc3136eda7
John E Vincent <lusis.org+github.com@gmail.com>
2017-02-22 10:25:28 -0500

    fixing a small issue with error handling returning the error. backwards compatible

--- src/artifactory.v491/errors.go
+++ src/artifactory.v491/errors.go
@@ -1,7 +1,8 @@
 package artifactory
 
 type ErrorsJson struct {
-	Errors []ErrorJson `json:"errors"`
+	Errors []ErrorJson `json:"errors,omitempty"`
+	Error  string      `json:"error,omitempty"`
 }
 
 type ErrorJson struct {

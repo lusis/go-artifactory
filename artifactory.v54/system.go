c388e9ca9fa792bf05de11d12bf2d8f46a80f42a
Jordan Brockopp <Jordan.Brockopp@target.com>
2017-08-28 13:07:24 -0500

    fixing typos. updating methods for 5.4

--- artifactory.v54/system.go
+++ artifactory.v54/system.go
@@ -5,3 +5,9 @@ func (c *Client) GetGeneralConfiguration() (s string, e error) {
 	d, e := c.Get("/api/system/configuration", make(map[string]string))
 	return string(d), e
 }
+
+// GetSystemHealthPing returns a simple status response about the state of Artifactory
+func (c *Client) GetSystemHealthPing() (s string, e error) {
+	d, e := c.Get("/api/system/ping", make(map[string]string))
+	return string(d), e
+}

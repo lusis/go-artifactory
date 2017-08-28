48e8e064327cd8fd23f42f592615d99fd11502e0
Jordan Brockopp <jdbro94@gmail.com>
2017-08-28 08:51:41 -0500

    add method to return http response from server (#12)
    * add method to return http response from server
    
    * fixing linting errors
    
    * add missing parse in delete. change method name to more logical

--- artifactory.v51/http.go
+++ artifactory.v51/http.go
@@ -34,33 +34,77 @@ func (c *Client) HTTPRequest(ar Request) ([]byte, error) {
 	for q, p := range ar.QueryParams {
 		options[q] = p
 	}
+	r, err := c.makeRequest(ar.Verb, ar.Path, options, ar.Body)
+	if err != nil {
+		var data bytes.Buffer
+		return data.Bytes(), err
+	}
+
+	return c.parseResponse(r)
+}
+
+// HTTPRequestWithResponse performs an HTTP request to artifactory and returns
+// the http response
+func (c *Client) HTTPRequestWithResponse(ar Request) (*http.Response, error) {
+	options := make(map[string]string)
+	if ar.ContentType != "" {
+		options["content-type"] = ar.ContentType
+	}
+	for q, p := range ar.QueryParams {
+		options[q] = p
+	}
+
 	return c.makeRequest(ar.Verb, ar.Path, options, ar.Body)
 }
 
 // Get performs an http GET to artifactory
 func (c *Client) Get(path string, options map[string]string) ([]byte, error) {
-	return c.makeRequest("GET", path, options, nil)
+	r, err := c.makeRequest("GET", path, options, nil)
+	if err != nil {
+		var data bytes.Buffer
+		return data.Bytes(), err
+	}
+
+	return c.parseResponse(r)
 }
 
 // Post performs an http POST to artifactory
 func (c *Client) Post(path string, data string, options map[string]string) ([]byte, error) {
 	body := strings.NewReader(data)
-	return c.makeRequest("POST", path, options, body)
+	r, err := c.makeRequest("POST", path, options, body)
+	if err != nil {
+		var data bytes.Buffer
+		return data.Bytes(), err
+	}
+
+	return c.parseResponse(r)
 }
 
 // Put performs an http PUT to artifactory
 func (c *Client) Put(path string, data string, options map[string]string) ([]byte, error) {
 	body := strings.NewReader(strings.TrimSuffix(data, "\n"))
-	return c.makeRequest("PUT", path, options, body)
+	r, err := c.makeRequest("PUT", path, options, body)
+	if err != nil {
+		var data bytes.Buffer
+		return data.Bytes(), err
+	}
+
+	return c.parseResponse(r)
 }
 
 // Delete performs an http DELETE to artifactory
 func (c *Client) Delete(path string) error {
-	_, err := c.makeRequest("DELETE", path, make(map[string]string), nil)
+	r, err := c.makeRequest("DELETE", path, make(map[string]string), nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.parseResponse(r)
+
 	return err
 }
 
-func (c *Client) makeRequest(method string, path string, options map[string]string, body io.Reader) ([]byte, error) {
+func (c *Client) makeRequest(method string, path string, options map[string]string, body io.Reader) (*http.Response, error) {
 	qs := url.Values{}
 	var contentType string
 	for q, p := range options {
@@ -71,20 +115,14 @@ func (c *Client) makeRequest(method string, path string, options map[string]stri
 			qs.Add(q, p)
 		}
 	}
-	// swapped out legacy code below for simply trimming the trailing slash
-	//if c.Config.BaseURL[:len(c.Config.BaseURL)-1] == "/" {
-	//	base_req_path = c.Config.BaseURL + path
-	//} else {
-	//	base_req_path = c.Config.BaseURL + "/" + path
-	//}
+
 	baseReqPath := strings.TrimSuffix(c.Config.BaseURL, "/") + path
 	if os.Getenv("ARTIFACTORY_DEBUG") != "" {
 		log.Printf("Final URL: %s", baseReqPath)
 	}
 	u, err := url.Parse(baseReqPath)
 	if err != nil {
-		var data bytes.Buffer
-		return data.Bytes(), err
+		return nil, err
 	}
 	if len(options) != 0 {
 		u.RawQuery = qs.Encode()
@@ -116,11 +154,13 @@ func (c *Client) makeRequest(method string, path string, options map[string]stri
 			log.Printf("Body: %#v", string(buf.Bytes()))
 		}
 	}
+
 	r, err := c.Client.Do(req)
-	if err != nil {
-		var data bytes.Buffer
-		return data.Bytes(), err
-	}
+
+	return r, err
+}
+
+func (c *Client) parseResponse(r *http.Response) ([]byte, error) {
 	defer func() { _ = r.Body.Close() }()
 	data, err := ioutil.ReadAll(r.Body)
 	if r.StatusCode < 200 || r.StatusCode > 299 {

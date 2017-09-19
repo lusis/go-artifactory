55d8dd6d8e854c2a53ceaf898ef73d87dd6a8ef8
Jordan Brockopp <jdbro94@gmail.com>
2017-09-19 11:56:38 -0500

    License and System (#18)
    * adding license methods
    
    * adding tests for license methods
    
    * adding test json file for license tests
    
    * adding system methods
    
    * adding tests for system methods
    
    * adding mock files for system tests

--- artifactory.v54/license_test.go
+++ artifactory.v54/license_test.go
@@ -1,6 +1,8 @@
 package artifactory
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -12,8 +14,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetSystemInfo(t *testing.T) {
-	responseFile, err := os.Open("assets/test/system_information.txt")
+func TestGetLicenseInfo(t *testing.T) {
+	responseFile, err := os.Open("assets/test/license_information.json")
 	if err != nil {
 		t.Fatalf("Unable to read test data: %s", err.Error())
 	}
@@ -41,16 +43,21 @@ func TestGetSystemInfo(t *testing.T) {
 	}
 
 	client := NewClient(conf)
-	info, err := client.GetSystemInfo()
+	info, err := client.GetLicenseInfo()
 	assert.NoError(t, err, "should not return an error")
-	assert.Equal(t, string(responseBody), info, "should return system information")
+	assert.Equal(t, "Trial", info.LicenseType, "Should have the Trial license type")
+	assert.Equal(t, "Sep 30, 2015", info.ValidThrough, "Should be valid through Sep 30th, 2015")
+	assert.Equal(t, "User Bob", info.LicensedTo, "Should be licensed to User Bob")
 }
 
-func TestGetSystemHealthPing(t *testing.T) {
+func TestInstallLicense(t *testing.T) {
+	var buf bytes.Buffer
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintf(w, "OK")
+		req, _ := ioutil.ReadAll(r.Body)
+		buf.Write(req)
+		fmt.Fprintf(w, "")
 	}))
 	defer server.Close()
 
@@ -69,13 +76,19 @@ func TestGetSystemHealthPing(t *testing.T) {
 	}
 
 	client := NewClient(conf)
-	health, err := client.GetSystemHealthPing()
+
+	license := InstallLicense{
+		LicenseKey: "179b7ea384d0c4655a00dfac7285a21d986a17923",
+	}
+
+	expectedJSON, _ := json.Marshal(license)
+	err := client.InstallLicense(license, make(map[string]string))
 	assert.NoError(t, err, "should not return an error")
-	assert.Equal(t, "OK", health, "should return ok ping health check")
+	assert.Equal(t, string(expectedJSON), string(buf.Bytes()), "should send license json")
 }
 
-func TestGetGeneralConfiguration(t *testing.T) {
-	responseFile, err := os.Open("assets/test/general_configuration.xml")
+func TestGetHALicenseInfo(t *testing.T) {
+	responseFile, err := os.Open("assets/test/ha_license_information.json")
 	if err != nil {
 		t.Fatalf("Unable to read test data: %s", err.Error())
 	}
@@ -103,22 +116,26 @@ func TestGetGeneralConfiguration(t *testing.T) {
 	}
 
 	client := NewClient(conf)
-	config, err := client.GetGeneralConfiguration()
+	info, err := client.GetHALicenseInfo()
 	assert.NoError(t, err, "should not return an error")
-	assert.Equal(t, string(responseBody), config, "should return general configuration")
+	assert.Len(t, info.Licenses, 2, "Should have 2 licenses")
+	assert.Equal(t, "Enterprise", info.Licenses[0].LicenseType, "Should have the Enterprise license type")
+	assert.Equal(t, "May 15, 2018", info.Licenses[0].ValidThrough, "Should be valid through May 15, 2018")
+	assert.Equal(t, "JFrog", info.Licenses[0].LicensedTo, "Should be licensed to JFrog")
+	assert.Equal(t, "179b7ea384d0c4655a00dfac7285a21d986a17923", info.Licenses[0].LicenseHash, "Should have 179b7ea384d0c4655a00dfac7285a21d986a17923 license hash")
+	assert.Equal(t, "art1", info.Licenses[0].NodeID, "Should have art1 node id")
+	assert.Equal(t, "http://10.1.16.83:8091/artifactory", info.Licenses[0].NodeURL, "Should have http://10.1.16.83:8091/artifactory node url")
+	assert.Equal(t, false, info.Licenses[0].Expired, "Should not be expired")
 }
 
-func TestGetVersionAndAddOnInfo(t *testing.T) {
-	responseFile, err := os.Open("assets/test/version_information.json")
-	if err != nil {
-		t.Fatalf("Unable to read test data: %s", err.Error())
-	}
-	defer func() { _ = responseFile.Close() }()
-	responseBody, _ := ioutil.ReadAll(responseFile)
+func TestInstallHALicense(t *testing.T) {
+	var buf bytes.Buffer
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintf(w, string(responseBody))
+		req, _ := ioutil.ReadAll(r.Body)
+		buf.Write(req)
+		fmt.Fprintf(w, "")
 	}))
 	defer server.Close()
 
@@ -137,10 +154,14 @@ func TestGetVersionAndAddOnInfo(t *testing.T) {
 	}
 
 	client := NewClient(conf)
-	info, err := client.GetVersionAndAddOnInfo()
+
+	licenses := []InstallLicense{
+		{LicenseKey: "179b7ea384d0c4655a00dfac7285a21d986a17923"},
+		{LicenseKey: "e10b8aa1d1dc5107439ce43debc6e65dfeb71afd3"},
+	}
+
+	expectedJSON, _ := json.Marshal(licenses)
+	err := client.InstallHALicenses(licenses, make(map[string]string))
 	assert.NoError(t, err, "should not return an error")
-	assert.Equal(t, "5.4.6", info.Version, "should return 5.4.6 version")
-	assert.Equal(t, "50406900", info.Revision, "should return 50406900 revision")
-	assert.Len(t, info.Addons, 8, "should have 8 strings in the addons array")
-	assert.Equal(t, "build", info.Addons[0], "should return build as first add on")
+	assert.Equal(t, string(expectedJSON), string(buf.Bytes()), "should send license json")
 }

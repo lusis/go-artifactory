b81a13ede288d459728f3db70ee521c8aed4ec63
John E Vincent <lusis.org+github.com@gmail.com>
2018-03-04 17:05:24 -0500

    travis testing commit

---
+++ pkg/artifactory/helpers_integration_test.go
@@ -0,0 +1,24 @@
+package artifactory_test
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+func testShouldRunIntegrationTests() bool {
+	// are the env vars set?
+	if os.Getenv("ARTIFACTORY_TOKEN") == "" || os.Getenv("ARTIFACTORY_URL") == "" {
+		return false
+	}
+	// are we pointing to our test instance?
+	return strings.Contains(os.Getenv("ARTIFACTORY_URL"), "lusis.jfrog.io")
+}
+
+func TestShouldRun(t *testing.T) {
+	spew.Dump(os.Environ())
+	fmt.Printf("%t\n", testShouldRunIntegrationTests())
+}
---
+++ pkg/artifactory/repositories_integration_test.go
@@ -0,0 +1,1 @@
+package artifactory_test
